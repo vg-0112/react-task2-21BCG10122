f5c8bf66f93b70a26082fa478995e8c262415b50
vg-0112 <117620979+vg-0112@users.noreply.github.com>
2023-11-10 20:42:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Calculator from './Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Simple Calculator</h1>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
